09ca28e7bec20a2b6b14edd0cbfeb6245d9c6146
Sean Sabe <sa.sb@outlook.com>
2023-08-06 17:32:41 -0700

    Service schema with required values

--- models/Service.js
+++ models/Service.js
@@ -1,6 +1,7 @@
 const { ObjectId } = require("mongodb");
 const mongoose = require("mongoose");
 
+/*
 const serviceSchema = new mongoose.Schema({
   userId: ObjectId, // ex. "5f8f8a8a8a8a8a8a8a8a8a8a"
   name: String, // ex. "Haircut"
@@ -14,3 +15,20 @@ const serviceSchema = new mongoose.Schema({
 serviceSchema.index({ name: "text", description: "text" });
 
 module.exports = mongoose.model("Service", serviceSchema);
+
+*/
+
+const Service = new mongoose.Schema(
+  {
+    userId: { type: ObjectId, required: true },
+    name: { type: String, required: true },
+    description: { type: String, required: true },
+    price: { type: Number, required: true },
+    kindOfService: { type: String, required: true }
+  },
+  {
+    collection: 'services'
+  }
+);
+
+module.exports = mongoose.model("Service", Service);
