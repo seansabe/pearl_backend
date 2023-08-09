25311573ef50887f9b18d11f17ecbfc10aebc4e0
Sergio Salazar <sa.sb@outlook.com>
2023-08-08 20:01:12 -0700

    Fix file name

--- routes/loginRoutes.js
+++ routes/loginRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const loginController = require('../controllers/LoginController');
+const loginController = require('../controllers/loginController');
 const registerController = require('../controllers/RegisterController');
 
 // Login route
