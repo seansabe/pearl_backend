3f4e61ced4c2cae4a7440a1b6f4b4c618d84a979
Sergio Salazar <sa.sb@outlook.com>
2023-08-08 19:16:22 -0700

    Missing Capitals in some file names

--- routes/searchRoutes.js
+++ routes/searchRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const { searchServices } = require("../controllers/searchController");
+const { searchServices } = require("../controllers/SearchController");
 
 router.get("/", searchServices);
 
--- routes/serviceRoutes.js
+++ routes/serviceRoutes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const serviceController = require("../controllers/ServiceController");
-const { filterByCategory } = require("../controllers/categoryController");
+const { filterByCategory } = require("../controllers/CategoryController");
 
 router.post("/", serviceController.createService);
 
